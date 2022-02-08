2962c98d680328ac207e5117153d2317aa575eda
LucaSicu <98978018+LucaSicu@users.noreply.github.com>
2022-02-08 12:42:21 -0500

    Add files via upload

---
+++ HW0/code/lucasic_module.py
@@ -0,0 +1,33 @@
+# Luca's module contains Luca's functions
+
+import numpy as np
+
+def power_number(x, n):
+	"""
+    Luca's first function.
+
+    Args:
+        x (float): Some number.
+        n (int): Natural number. 
+
+    Returns:
+        float: Returns :math:`y=x^n`
+    """
+
+	return x**n
+
+def times_by_n(x, n):
+	"""
+    Luca's second function.
+
+    Args:
+        x (float): Some number.
+        n (int): Natural number
+
+    Returns:
+        float: Returns :math:`y=nx`
+    """
+
+	return n*x
+
+	
