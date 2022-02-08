fee72f693952d5e9fd8a5e854eef9c08578348d7
Temitope Benson <68304751+Temitope746@users.noreply.github.com>
2022-02-07 21:27:54 -0500

    Updating temitopes function in temitopes_module

--- HW0/code/temitopes_module.py
+++ HW0/code/temitopes_module.py
@@ -16,39 +16,17 @@ def square_a_number_add_2(x):
 	return x**2 + 2
 
 
-def pipe_flow(diameter, depth, slope, roughness_n= 0.045):
-        
-    """
+def times_by_5(x):
+	"""
     Temitope Benson's second function.
 
-    Calculates flow in a pipe.
-
     Args:
-        diameter: Pipe diameter in inches or cm.
-        depth: Depth of water in inches or cm.
-        slope: Slope of the pipe in ft/ft or m/m
-        roughness_n: Manning's roughness, default corrugated metal pipe
+        x (float): Some number.
 
     Returns:
-         Q: Flow in cfs or m^3/sec
+        float: Returns :math:`y=5x`
     """
 
-   # Assign variables.
-     c = 1.49
-     D = diameter/12
-     d = depth/12
-     r = D/2
-     n = roughness_n
-     S = slope
+	return 5*x
 
-    # Intermediate calculations.
-     theta = 2 * np.arccos((r-d)/r)
-     A = ((r**2) * (theta - np.sin(theta)))/2  # Water flow area.
-     P = theta * r  # Wetted perimet    
-     Rh = A/P  # Hydraulic radius
-    # Final calculations.
-     v = c/n * Rh**(2/3) * S**0.5
-     Q = v * A
-
-     return Q
 	
