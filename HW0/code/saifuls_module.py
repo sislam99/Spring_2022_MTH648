4022410e949a64e728cc3929b5a68842785002e6
sislam99 <98930432+sislam99@users.noreply.github.com>
2022-02-09 20:01:18 -0500

    saifuls_module

---
+++ HW0/code/saifuls_module.py
@@ -0,0 +1,36 @@
+
+
+import numpy as np
+
+def average_of_two_numbers(x,y):
+	"""
+    A function calculating the average of two numbers.
+
+    Args:
+        x (float): Some number.
+        y (float): Some number.
+
+    Returns:
+        float: Returns :math:`average=(x+y)/2`
+    """
+    
+	avg=(x+y)/2
+
+	return avg
+
+def sum_of_three_numbers(x,y,z):
+	"""
+    Second function to find the summation of three numbers.
+
+    Args:
+        x (float): Some number.
+        y (float): Some number.
+        z (float): Some number.
+
+    Returns:
+        float: Returns :math:`s=x+y+z`
+    """
+    
+    
+
+	return x+y+z
